0cdf3efdd63459faa7b9d55ead20a65eaf666d93
Abhishek Prajapati <104263740+abhiprj@users.noreply.github.com>
2023-12-18 22:03:09 +0530

    Game of XOR
    Given an array A of size N. The value of an array is denoted by the bit-wise XOR of all elements it contains. Find the bit-wise XOR of the values of all subarrays of A.
    
    Example 1:
    
    Input: 
    N = 3 
    A = [1,2,3] 
    Output: 
    2
    Explanation:
    xor[1] = 1
    xor[1, 2] = 3
    xor[2, 3] = 1
    xor[1, 2, 3] = 0
    xor[2] = 2
    xor[3] = 3
    Result : 1 ^ 3 ^ 1 ^ 0 ^ 2 ^ 3 = 2
    Example 2:
    
    Input: 
    N = 2
    A = [1,2]
    Output: 
    0
    Explanation:
    xor[1] = 1
    xor[1, 2] = 3
    xor[2] = 2
    Result : 1 ^ 3 ^ 2 = 0
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function gameOfXor() which takes an integer N and array A[] as input parameters and returns the answer.
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 <= N <= 105
    0 <= A[i] <= 109

---
+++ Game of XOR.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int gameOfXor(int N , int A[]) {
+        if (N % 2 == 0)
+            return 0;
+
+        int XOR = 0;
+        for (int i = 0; i < N; i += 2)
+            XOR ^= A[i];
+        return XOR;
+    }
+};
