8adaca9cf01e6007139509e5acc608820ed17d10
Abhishek Prajapati <104263740+abhiprj@users.noreply.github.com>
2024-01-10 09:00:11 +0530

    Longest subarray with sum divisible by K
    Given an array arr containing N integers and a positive integer K, find the length of the longest sub array with sum of the elements divisible by the given value K.
    
    Example 1:
    
    Input:
    N = 6, K = 3
    arr[] = {2, 7, 6, 1, 4, 5}
    Output: 
    4
    Explanation:
    The subarray is {7, 6, 1, 4} with sum 18, which is divisible by 3.
    Example 2:
    
    Input:
    N = 7, K = 3
    arr[] = {-2, 2, -5, 12, -11, -1, 7}
    Output: 
    5
    Explanation:
    The subarray is {2,-5,12,-11,-1} with sum -3, which is divisible by 3.
    Your Task:
    The input is already taken care of by the driver code. You only need to complete the function longSubarrWthSumDivByK() that takes an array arr, sizeOfArray n and a  positive integer K, and returns the length of the longest subarray which has sum divisible by K. 
    
    Expected Time Complexity: O(N).
    Expected Auxiliary Space: O(N).
    
    Constraints:
    1 <= N <= 105
    1 <= K <= 109
    -109 <= A[i] <= 109

---
+++ Longest subarray with sum divisible by K.java
@@ -0,0 +1,21 @@
+class Solution{
+    int longSubarrWthSumDivByK(int a[], int n, int k)
+    {
+        // Complete the function
+        HashMap<Integer,Integer> mp= new HashMap<>();
+        int mx=0,r=0,s=0;
+       mp.put(0,-1);
+        for(int i=0;i<n;i++){
+            s+=a[i];
+            r=s%k;
+            if(r<0)r+=k;
+            
+            if(mp.containsKey(r)){
+                mx=Math.max(mx,i-mp.get(r));
+                continue;
+            }
+            mp.put(r,i);
+        }
+       return mx;
+    }
+}
