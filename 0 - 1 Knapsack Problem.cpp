39582a180ded2a85a3e65e810ca276b1f7ed1586
Abhishek Prajapati <104263740+abhiprj@users.noreply.github.com>
2024-01-13 16:21:04 +0530

    0 - 1 Knapsack Problem
    You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
    In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item or dont pick it (0-1 property).
    
    Example 1:
    
    Input:
    N = 3
    W = 4
    values[] = {1,2,3}
    weight[] = {4,5,1}
    Output: 3
    Explanation: Choose the last item that weighs 1 unit and holds a value of 3. 
    Example 2:
    
    Input:
    N = 3
    W = 3
    values[] = {1,2,3}
    weight[] = {4,5,6}
    Output: 0
    Explanation: Every item has a weight exceeding the knapsack's capacity (3).
    Your Task:
    Complete the function knapSack() which takes maximum capacity W, weight array wt[], value array val[], and the number of items n as a parameter and returns the maximum possible value you can get.
    
    Expected Time Complexity: O(N*W).
    Expected Auxiliary Space: O(N*W)
    
    Constraints:
    1 ≤ N ≤ 1000
    1 ≤ W ≤ 1000
    1 ≤ wt[i] ≤ 1000
    1 ≤ v[i] ≤ 1000

---
+++ 0 - 1 Knapsack Problem.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int dp[1001][1001]; 
+    int solve(int w,vector<int>&v,vector<int>&u,int n,int i){
+        if(i>=n)
+        return 0; 
+        if(w<=0)
+        return 0; 
+        if(dp[i][w]!=-1)
+        return dp[i][w]; 
+        if(v[i]<=w)
+        return dp[i][w]=max(solve(w-v[i],v,u,n,i+1)+u[i],solve(w,v,u,n,i+1)); 
+        else
+        return dp[i][w]=solve(w,v,u,n,i+1); 
+       // if(w-v[i])
+        //take
+        // solve(w-u[i],v,u,n,i+1,sum +v[i]); 
+        //don't take
+        //return dp[i][w]=max(solve(w-v[i],v,u,n,i+1)+u[i],solve(w,v,u,n,i+1)); 
+    }
+    int knapSack(int w, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       memset(dp,-1,sizeof(dp));
+       vector<int>v; 
+       vector<int>u;
+       for(int i=0;i<n;i++){
+           v.push_back(wt[i]); 
+       }
+       for(int i=0;i<n;i++){
+           u.push_back(val[i]); 
+       }
+       int sum=0; 
+    //   solve(w,v,u,n,0,sum); 
+       return solve(w,v,u,n,0);
+    }
+};
