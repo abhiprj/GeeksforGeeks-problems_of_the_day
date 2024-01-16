5c2dadf2b64a39467077e6dc1a726428f4e78e3a
Abhishek Prajapati <104263740+abhiprj@users.noreply.github.com>
2024-01-17 01:40:54 +0530

    Perfect Sum Problem
    Given an array arr of non-negative integers and an integer sum, the task is to count all subsets of the given array with a sum equal to a given sum.
    
    Note: Answer can be very large, so, output answer modulo 109+7.
    
    Example 1:
    
    Input: 
    N = 6
    arr = [5, 2, 3, 10, 6, 8]
    sum = 10
    Output: 
    3
    Explanation: 
    {5, 2, 3}, {2, 8}, {10} are possible subsets.
    Example 2:
    Input: 
    N = 5
    arr = [2, 5, 1, 4, 3]
    sum = 10
    Output: 
    3
    Explanation: 
    {2, 1, 4, 3}, {5, 1, 4}, {2, 5, 3} are possible subsets.
    Your Task:  
    You don't need to read input or print anything. Complete the function perfectSum() which takes N, array arr and sum as input parameters and returns an integer value.
    
    Expected Time Complexity: O(N*sum)
    Expected Auxiliary Space: O(N*sum)
    
    Constraints:
    1 ≤ N*sum ≤ 106
    0 ≤ arr[i] ≤ 106

---
+++ Perfect Sum Problem.java
@@ -0,0 +1,20 @@
+class Solution{
+    public static int mod = 1000000007;
+    public static int f(int arr[],int n,int sum,int dp[][]){
+        if(n==0){
+            if(sum==0) return 1;
+            return 0;
+        }
+        if(dp[n][sum]!=-1) return dp[n][sum];
+        if(sum>=arr[n-1]){
+            return dp[n][sum] = (f(arr,n-1,sum-arr[n-1],dp)%mod + f(arr,n-1,sum,dp)%mod)%mod;
+        }
+        return dp[n][sum] = f(arr,n-1,sum,dp)%mod;
+    }
+
+	public int perfectSum(int arr[],int n, int sum) { 
+	    int dp[][] = new int[n+1][sum+1];
+	    for(int row[]:dp) Arrays.fill(row,-1);
+	    return f(arr,n,sum,dp)%mod;
+	} 
+}
