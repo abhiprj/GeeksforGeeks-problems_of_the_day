6833383e7d29e6ab2211748222d4bea15aa9ae1d
Abhishek Prajapati <104263740+abhiprj@users.noreply.github.com>
2024-01-27 14:43:53 +0530

    Add 1 to a number represented as linked list
    A number N is represented in Linked List such that each digit corresponds to a node in linked list. You need to add 1 to it.
    
    Example 1:
    
    Input:
    LinkedList: 4->5->6
    Output: 457
    Explanation: 4->5->6 represents 456 and when 1 is added it becomes 457. 
    Example 2:
    
    Input:
    LinkedList: 1->2->3
    Output: 124 
    Your Task:
    Your task is to complete the function addOne() which takes the head of the linked list as the only argument and returns the head of the modified linked list. The driver code prints the number.
    Note: The head represents the left-most digit of the number.
    
    Expected Time Complexity: O(N).
    Expected Auxiliary Space: O(1).
    
    Constraints:
    1 <= N <= 1021

---
+++ Add 1 to a number represented as linked list.py
@@ -0,0 +1,36 @@
+
+'''
+
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def reverse(self,head):
+        prev=None
+        curr=head
+        while curr:
+            next=curr.next
+            curr.next=prev
+            prev=curr
+            curr=next
+        return prev
+        
+    def addOne(self, head):
+        curr=head
+        n=0
+        while curr:
+            n=curr.data+n*10
+            curr=curr.next
+        n=n+1
+        newhead=Node(n%10)
+        n=n//10
+        temp=newhead
+        while n:
+            newhead.next=Node(n%10)
+            newhead=newhead.next
+            n=n//10
+        newhead=self.reverse(temp)
+        return newhead
